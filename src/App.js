20a855e7424b2faa2d003858d5aab322f9451f49
Your Name <you@example.com>
2024-04-21 19:47:52 +0800

    aga

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Monkeys from './components/Monkeys';
+import Justification from './components/Justification';
+import AntonSyperCritoi from './components/AntonSyperCritoi';
+
+function App() {
+  return (
+    <div className="App">
+        <Justification />
+        <Monkeys/>
+        <AntonSyperCritoi />
+    </div>
+  );
+}
+
+export default App;
